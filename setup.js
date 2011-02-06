8b3a844bbb5ffbe34860d377323bc92b276cf2cc
AndrewVos <andrew.vos@gmail.com>
2011-02-06 19:28:47 +0000

    Added some input elements to the demo page

--- setup.js
+++ setup.js
@@ -1,7 +1,7 @@
 $(document).ready(randomize);
 
 function randomize(){
-  $("a").each(function(index, element){
+  $("a, input").each(function(index, element){
       var offset = {left: Math.random() * $(window).width(), top: Math.random() * $(window).height()};
       $(element).css('position', 'absolute');
       $(element).offset(offset);
