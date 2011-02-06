c02953732a5a6f6810a94071ae4c00b37cebe211
AndrewVos <andrew.vos@gmail.com>
2011-02-06 22:48:48 +0000

    Removed unneeded index.html files and moved all scripts to the js folder

--- setup.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(randomize);
-
-function randomize(){
-  $("a, input").each(function(index, element){
-      var offset = {left: Math.random() * $(window).width(), top: Math.random() * $(window).height()};
-      $(element).css('position', 'absolute');
-      $(element).offset(offset);
-  });
-}
